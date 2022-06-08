ecb2f993480d876298544a006a6b17d2c3a21bd9
rhaf21 <106423415+rhaf21@users.noreply.github.com>
2022-06-08 19:45:10 +0800

    Create rgb.js

---
+++ rgb.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById('btn');
+const color = document.getElementById('color');
+const body = document.querySelectorAll('body');
+const copy = document.getElementById('copy');
+
+btn.addEventListener('click', function () {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let rgbColor = `RGB(${red},${green},${blue})`
+    
+    document.getElementById('rgbValue').value = rgbColor;
+    document.body.style.background = rgbColor;
+    color.textContent = rgbColor;
+    
+});
+
+copy.addEventListener('click', function (){
+    let rgb = document.getElementById('rgbValue');
+    
+    rgb.select();
+    rgb.setSelectionRange(0, 99999); /* For mobile devices */
+
+    navigator.clipboard.writeText(rgb.value);
+  
+    alert("Copied the text: " + rgb.value);
+});
+    
